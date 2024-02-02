1609b666ef1e1c08938649470e5d87df6a58263f
Luciana Gomes <lucianasousa@uninove.edu.br>
2024-02-01 21:41:30 -0300

    separado Js do HTML

---
+++ scripts.js
@@ -0,0 +1,12 @@
+    let formu = document.querySelector(".formulario");
+
+    function cliquei(){
+    formu.style.left = "1700px"; 
+    }
+
+    function sumir(){
+        formu.style.left = "-700px"
+
+    }
+
+    
